8b6e9451ad8eeb87382ffb31905b2de6969607de
emilhrdnsy <emilhardiansyah54@gmail.com>
2021-08-31 13:51:02 +0800

    first commit

---
+++ ci4app.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 31 Agu 2021 pada 07.50
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `komik`
+--
+
+CREATE TABLE `komik` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `sampul` varchar(255) NOT NULL,
+  `jilid` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `komik`
+--
+
+INSERT INTO `komik` (`id`, `judul`, `slug`, `penulis`, `penerbit`, `sampul`, `jilid`, `created_at`, `updated_at`) VALUES
+(1, 'Black Clover', 'black-clover', 'Yuki Tabata', 'Shueisha', 'Volume_27.jpg', 'Volume 27', '2021-08-29 15:17:21', '2021-08-29 15:17:21'),
+(2, 'Black Clover', 'black-clover', 'Yuki Tabata', 'Shueisha', 'Volume_28.jpg', 'Volume 28', '2021-08-29 15:20:38', '2021-08-29 15:20:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `komik`
+--
+ALTER TABLE `komik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `komik`
+--
+ALTER TABLE `komik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
